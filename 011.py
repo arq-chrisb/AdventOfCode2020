d2a5d88c95beeda62145b71ca0e5d7c170c55145
Chris Ballment <chris.ballment@arq.group>
2020-12-11 18:57:21 +1100

    Optimisations

--- 011.py
+++ 011.py
@@ -1,4 +1,4 @@
-adjacent_seats = [(0, -1),(1, -1),(1, 0),(1,1), (0, 1), (-1,1), (-1, 0), (-1, -1)]
+adjacent_seats = [(x, y) for x in range(-1, 2) for y in range(-1, 2) if not (x == 0 and y == 0)]
 
 def count_adjacent(x,y, waiting_area):
     len_x = len(waiting_area[0])
@@ -17,8 +17,8 @@ def count_line_of_sight(x,y, waiting_area):
     count = 0
     for diff_x, diff_y in adjacent_seats:
         new_x, new_y = x + diff_x, y + diff_y
-        # while it's valid and not containing a person
-        while (0 <= (new_x) < len_x) and (0 <= (new_y) < len_y):
+        # while it's valid
+        while (0 <= new_x < len_x) and (0 <= new_y < len_y):
             if waiting_area[new_y][new_x] == '#':
                 count += 1
                 break
@@ -31,15 +31,14 @@ def count_line_of_sight(x,y, waiting_area):
 def cycle(waiting_area, seat_amt, leave_amt, adj_func):
     new_waiting_area = [line[:] for line in waiting_area]
 
-    for y in range(len(waiting_area)):
-        row = waiting_area[y]
-        for x in range(len(row)):
+    for y, row in enumerate(waiting_area):
+        for x, seat in enumerate(row):
             adj = adj_func(x, y, waiting_area)
-            if row[x] == 'L' and adj == seat_amt:
+            if seat == 'L' and adj == seat_amt:
                 new_waiting_area[y][x] = '#'
-            elif row[x] == "#" and adj >= leave_amt:
+            elif seat == "#" and adj >= leave_amt:
                 new_waiting_area[y][x] = 'L'
-    
+
     return new_waiting_area
 
 def print_waiting_area(waiting_area):
